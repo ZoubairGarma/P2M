090ccd9ddacce011c4822cdcd7c5450f80ce2371
inesbesbes1 <ines.besbes@supcom.tn>
2026-05-24 19:25:34 +0100

    lastpush

--- esp32_cam/src/reseau.h
+++ esp32_cam/src/reseau.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <WebServer.h>
+
+extern WebServer server;
+extern const char* ssid;
+extern const char* password;
+extern bool rfidDetected;
+extern unsigned long rfidDetectedTimestamp;
 
 void initialiserWiFiEtCloud();
 void initialiserServeurWeb();
